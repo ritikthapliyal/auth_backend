455de874caef6f75cc553b94e188ea5494f4869b
ritikthapliyal <ritikthapliyal123@gmail.com>
2023-09-18 01:28:29 +0530

    started with portfolio in frontend

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -37,7 +37,7 @@ router.get('/credentials',isAuth,async (req, res) => {
     try{
         const {username,email} = req.user
         res.status(200).json({username,email})
-    }
+    } 
     catch(err){
         console.log(err)
         res.status(500).json({message:"internal server error"})
